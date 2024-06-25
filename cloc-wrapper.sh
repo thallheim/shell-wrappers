6707c21dc94d3c0ad8edf65585382c3591907da0
thallheim <thall@thallheim.com>
2024-06-25 20:05:21 +0200

    Add cloc wrapper

---
+++ cloc-wrapper.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# shellcheck disable=3020
+
+# Ensure sed is installed
+if ! command -v sed &> /dev/null
+then
+    echo "ERR: Can't find sed, somehow. Now what?"
+    exit 1
+fi
+
+# Ensure cloc is installed
+if ! command -v cloc &> /dev/null
+then
+    echo "ERR: Can't find cloc. Is it installed?"
+    exit 1
+fi
+
+# Call cloc and stash its output
+cloc_output=$(cloc "$@" 2>&1)
+
+# Extract GH link, version string, and stats
+attrib=$(echo "$cloc_output" | grep 'github.com/AlDanial/cloc v')
+
+# Strip link, version and stats
+cloc_output_stripped=$(echo "$cloc_output" | sed "/github.com\/AlDanial\/cloc v/d")
+
+# Reflow output
+echo ""
+echo "$attrib"
+echo "-------------------------------------------------------------------------------"
+echo "$cloc_output_stripped"
